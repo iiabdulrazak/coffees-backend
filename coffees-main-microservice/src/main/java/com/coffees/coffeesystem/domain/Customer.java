ef23ffc122b46415a193ed3b6ca70d73535ca496
Abdulrazak A. Osman <zaak0101@hotmail.com>
2022-02-09 09:29:33 +0300

    New Entity (Customer)

---
+++ coffees-main-microservice/src/main/java/com/coffees/coffeesystem/domain/Customer.java
@@ -0,0 +1,101 @@
+package com.coffees.coffeesystem.domain;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+/**
+ * A Customer.
+ */
+@Entity
+@Table(name = "customer")
+public class Customer implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    @Column(name = "id")
+    private Long id;
+
+    @NotNull
+    @Size(min = 2, max = 35)
+    @Column(name = "customer_name", length = 35, nullable = false)
+    private String customerName;
+
+    @NotNull
+    @Size(min = 10, max = 10)
+    @Column(name = "customer_phone", length = 10, nullable = false, unique = true)
+    private String customerPhone;
+
+    // jhipster-needle-entity-add-field - JHipster will add fields here
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public Customer id(Long id) {
+        this.setId(id);
+        return this;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCustomerName() {
+        return this.customerName;
+    }
+
+    public Customer customerName(String customerName) {
+        this.setCustomerName(customerName);
+        return this;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerPhone() {
+        return this.customerPhone;
+    }
+
+    public Customer customerPhone(String customerPhone) {
+        this.setCustomerPhone(customerPhone);
+        return this;
+    }
+
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+
+    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Customer)) {
+            return false;
+        }
+        return id != null && id.equals(((Customer) o).id);
+    }
+
+    @Override
+    public int hashCode() {
+        // see https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
+        return getClass().hashCode();
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "Customer{" +
+            "id=" + getId() +
+            ", customerName='" + getCustomerName() + "'" +
+            ", customerPhone='" + getCustomerPhone() + "'" +
+            "}";
+    }
+}
---
+++ coffees-main-microservice/src/main/java/com/coffees/coffeesystem/repository/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.coffees.coffeesystem.repository;
+
+import com.coffees.coffeesystem.domain.Customer;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data SQL repository for the Customer entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {}
